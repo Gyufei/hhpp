f755190e5686a6e6f085169dd66c3141f4a7c215
fei guo <812773673@qq.com>
2025-01-15 15:49:37 +0800

    feat:balance-new-refetch

--- app/[locale]/_page-layout/_header/connect-btn.tsx
+++ app/[locale]/_page-layout/_header/connect-btn.tsx
@@ -4,19 +4,19 @@ import Image from "next/image";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useTranslations } from "next-intl";
 import { useChainWallet } from "@/lib/hooks/web3/use-chain-wallet";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useDeviceSize } from "@/lib/hooks/common/use-device-size";
 import { useWalletModalContext } from "@/components/provider/wallet-modal-provider";
 
+import BalancePopContent from "./balance-pop-content";
+import { useAccountEffect, useSignMessage } from "wagmi";
+import { isProduction } from "@/lib/PathMap";
+import HoverIcon from "@/components/share/hover-icon";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import BalancePopContent from "./balance-pop-content";
-import { useAccountEffect, useSignMessage } from "wagmi";
-import { isProduction } from "@/lib/PathMap";
-import HoverIcon from "@/components/share/hover-icon";
 
 const SignMessageKey = "hypes-trade-sign-message";
 
@@ -30,6 +30,9 @@ export default function ConnectBtn() {
   const { shortAddr, connected, connecting } = useChainWallet();
 
   const [popOpen, setPopOpen] = useState(false);
+  const balancePopRef = useRef<{
+    refetchBalance: () => void;
+  }>(null);
 
   function handleConnect() {
     openWalletModal(true);
@@ -56,6 +59,14 @@ export default function ConnectBtn() {
       localStorage.removeItem(SignMessageKey);
     },
   });
+
+  function handleOpenBalancePop(e: React.MouseEvent<HTMLDivElement>) {
+    e.stopPropagation();
+    if (!connected) return;
+
+    balancePopRef.current?.refetchBalance();
+    setPopOpen(true);
+  }
 
   if (!connected) {
     if (isMobileSize) {
@@ -84,7 +95,10 @@ export default function ConnectBtn() {
   return (
     <Popover open={popOpen} onOpenChange={(isOpen) => setPopOpen(isOpen)}>
       <PopoverTrigger asChild>
-        <div className="flex cursor-pointer items-center gap-[5px]">
+        <div
+          onClick={handleOpenBalancePop}
+          className="flex cursor-pointer items-center gap-[5px]"
+        >
           <div className="flex items-center text-xs leading-[18px] text-title-white hover:text-main">
             {!shortAddr || connecting ? (
               <Skeleton className="h-5 w-24" />
@@ -106,7 +120,7 @@ export default function ConnectBtn() {
         className="flex w-[276px] flex-col items-stretch space-y-[10px] border-border-black bg-bg-black p-[10px] text-[12px]"
         align="end"
       >
-        <BalancePopContent />
+        <BalancePopContent ref={balancePopRef} />
       </PopoverContent>
     </Popover>
   );
