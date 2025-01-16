c01b2dd545a8a3465ec66a142147d9e89be004ad
Derek Li <derekli2024@gmail.com>
2025-01-16 20:37:46 +0800

    fix: update sales size

--- app/[locale]/direct-trade/[...name]/chart/sales-chart.tsx
+++ app/[locale]/direct-trade/[...name]/chart/sales-chart.tsx
@@ -130,7 +130,7 @@ export default function SalesChart({
     () => ({
       chart: {
         backgroundColor: "transparent",
-        height: window.innerHeight > 900 ? "400px" : "249px",
+        height: window.innerHeight - 56 - 297 - 66 - 86 + "px",
       },
       time: {
         useUTC: false,
