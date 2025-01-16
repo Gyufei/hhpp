4383557f4bd38c330bfece4dbd7fb15445d30363
Derek Li <derekli2024@gmail.com>
2025-01-16 15:14:01 +0800

    fix: add taker api error tip

--- lib/hooks/contract/use-create-taker-order.ts
+++ lib/hooks/contract/use-create-taker-order.ts
@@ -3,6 +3,7 @@ import { dataApiFetcher } from "@/lib/fetcher";
 import useTxStatus from "@/lib/hooks/contract/help/use-tx-status";
 import { useChainWallet } from "@/lib/hooks/web3/use-chain-wallet";
 import { useSignData } from "./help/use-sign-data";
+import { toast } from "react-hot-toast";
 
 export function useCreateTakerOrder() {
   const { realAddress, address } = useChainWallet();
@@ -17,19 +18,23 @@ export function useCreateTakerOrder() {
       source_account: realAddress,
       dest_account: address,
     });
-
-    const res = await dataApiFetcher(
-      `${dataApiEndPoint}/offer/${offerId}/take`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
+    try {
+      const res = await dataApiFetcher(
+        `${dataApiEndPoint}/offer/${offerId}/take`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(reqData),
         },
-        body: JSON.stringify(reqData),
-      },
-    );
+      );
 
-    return res;
+      return res;
+    } catch (e: any) {
+      toast.error(e?.message || "The service is abnormal. Please try again");
+      return null;
+    }
   };
 
   const wrapRes = useTxStatus(txAction);
