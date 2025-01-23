441dcb431c200a4ef703ade9c1ed1d6b352bfc2d
Derek Li <derekli2024@gmail.com>
2025-01-24 05:51:18 +0800

    fix: del window.open

--- lib/utils/social.ts
+++ lib/utils/social.ts
@@ -1,30 +1,6 @@
-
 export const TwitterLink = "https://x.com/HypeTradeXYZ";
-export const DiscordLink = "https://discord.com/channels/1254632665685164092/1254724478038970409";
+export const DiscordLink =
+  "https://discord.com/channels/1254632665685164092/1254724478038970409";
 export const TgLink = "https://t.me/hypetradeXYZ";
 export const GithubLink = "https://github.com/HypeTradeXYZ";
-export const DocLink = "https://hypetrade.gitbook.io/hypetrade"
-
-export const handleGoTwitter = () => {
-  window.open(TwitterLink);
-};
-
-export const handleGoDiscord = () => {
-  // window.open(DiscordLink);
-};
-
-export const handleGoToDiscordTickets = () => {
-  // window.open(DiscordLink);
-}
-
-export const handleGoGithub = () => {
-
-};
-
-export const handleGoTg = () => {
-  // window.open(TgLink);
-};
-
-export const handleGoDoc = () => {
-  // window.open(DocLink);
-}
+export const DocLink = "https://hypetrade.gitbook.io/hypetrade";
