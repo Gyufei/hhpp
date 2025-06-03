80c5fb9c54fbbbb32d59b90484fca903bf0b7450
fei guo <812773673@qq.com>
2025-06-03 17:57:48 +0800

    fix:first-market

--- app/[locale]/direct-trade/page.tsx
+++ app/[locale]/direct-trade/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useRouter } from "@/i18n/routing";
 import { useMarketplaces } from "@/lib/hooks/api/use-marketplaces";
+import { checkIsAfterExpiry } from "@/lib/hooks/offer/use-offer-format";
 import { useSearchParams } from "next/navigation";
 
 export default function Marketplace() {
@@ -8,7 +9,9 @@ export default function Marketplace() {
   const { data: markets } = useMarketplaces();
   const searchParams = useSearchParams();
 
-  const firstMarket = markets?.[0];
+  const firstMarket = markets?.filter(
+    (m) => !checkIsAfterExpiry(m.expiry_date),
+  )?.[0];
 
   if (firstMarket) {
     router.replace(
