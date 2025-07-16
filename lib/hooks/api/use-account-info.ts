e986b5011b87242f7dd2706b3a873a0ef0378e06
fei guo <812773673@qq.com>
2025-07-16 17:14:52 +0800

    fix:typo

--- lib/hooks/api/use-account-info.ts
+++ lib/hooks/api/use-account-info.ts
@@ -6,12 +6,12 @@ export function useAccountInfo() {
   return {
     data: address
       ? {
-          // dest_account: address,
+          dest_account: address,
           // TODO: remove
           // #a
           // dest_account: "0xb6FA7f135038600E7071378Ac57cdb1e35e4936b",
           // #b
-          dest_account: "0xaE71F62Bfd81058a7c9024d729D79B9C20524Ec4",
+          // dest_account: "0xaE71F62Bfd81058a7c9024d729D79B9C20524Ec4",
           // #c
           // dest_account: "0x70Fa72840BabA86bc0296a74ce328b3Ac0646aeE",
         }
